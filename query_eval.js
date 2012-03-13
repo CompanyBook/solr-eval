3a868177f965e19974569eecf0d48477df159a15
birger lie <birger.lie@gmail.com>
2012-03-13 09:25:32 +0100

    working with excellent help from Martin

--- query_eval.js
+++ query_eval.js
@@ -1,45 +1,56 @@
-var url = '/select/?start=0&rows=105&wt=json&json.wrf=?&debugQuery=true';
-var search_result = ''
-var doc_fields = ['name','score', 'boost_revenue']
-var doc_id = 'company_id'
-var solr =  "datanode29.companybook.no:8080/solr/no_companies_20120207"
+var url = '/select/?start=0&rows=10&wt=json&json.wrf=?&debugQuery=true';
+var search_result = '';
+var doc_fields = ['name', 'score', 'boost_revenue', 'city'];
+var doc_id = 'company_id';
+var solr = "datanode29.companybook.no:8080/solr/no_companies_20120207";
 
+$(document).ready(function() {
+        alert('hey martin');
+console.log('document load');
 
-$(document).ready(function(){
-            
-    $("#search_button").click(function() {        
-        var query = $("#search_text").val()    
-        if(query.length == 0 ){
+    $('#search > form').submit(function(e) {
+        e.preventDefault();
+        console.log('clicked');
+
+        var query = $("#search_text").val()
+        if (query.length == 0) {
             alert('search for something...')
             return false
-        }        
-        $("#result_list").children().remove();                
-        alert('before q')
+        }
+        $("#result_list").children().remove();
+        console.log('before search call')
         $.ajax(
-            {
-                url:build_query(),
-                dataType:'json',
-                data:{},
-                success:function(result){
-                    search_result = result
+        {
+            url: build_query(),
+            dataType: 'json',
+            data: {},
+            success: function(result) {
+                console.log('search done')
+                search_result = result;
 
-                    display_header()
-                                
-                    for( var i = 0;i< result.response.docs.length;i++){
-                        display_document(result.response.docs[i])
-                    }
+                display_header();
+
+                for (var i = 0; i < result.response.docs.length; i++) {
+                    display_document(result.response.docs[i]);
+                }
+                console.log('end seach callback')
+                
+            },
+            error: function(e) {
+                alert('error performing search');
+                    search_complete = true;
                 }       
         }        
         );
     });
 });
-                   
+
 function build_query(){
         var query = $("#search_text").val()
-        
+
         var fl = "&fl="+ doc_id
-        $.each(doc_fields, function(index,val){fl += ','+ val })
-        
+        $.each(doc_fields, function(index,val){fl += ', '+ val })
+
         return  "http://" + solr + url +fl + "&q=" +  query
 }
 
@@ -47,11 +58,11 @@ function display_header(){
     var header = document.createElement('thead');
     var row =  document.createElement('tr');
     header.appendChild(row);
-    
+
     var th = document.createElement('th');
     th.appendChild(document.createTextNode(doc_id));
     row.appendChild(th)
-    
+
     for(i =0; i< doc_fields.length; i++){
         var th = document.createElement('th');
         th.appendChild(document.createTextNode(doc_fields[i]));
@@ -76,4 +87,3 @@ function display_document(doc){
 
 document.getElementById("result_list").appendChild(row);
 }
-		
